4dd52d48d908d85bd14be4a03528e230454720f4
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-28 18:24:58 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hackbox_2_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { :host => 'https://hackbox-app.herokuapp.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
