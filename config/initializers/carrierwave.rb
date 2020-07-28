5033b9f2b300894c6d0c9821cf8ae53137e3d6c5
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-28 22:32:02 +0900

    S3設定の変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,35 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      # Amazon S3用の設定
-      provider: 'AWS',
-      region: 'ap-northeast-1',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    }
-    config.fog_directory     =  'hackbox'
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hackbox'
-  end
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       # Amazon S3用の設定
+#       provider: 'AWS',
+#       region: 'ap-northeast-1',
+#       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#     }
+#     config.fog_directory     =  'hackbox'
+#     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+#     # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hackbox'
+#   end
 
-  # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+#   # 日本語ファイル名の設定
+#   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+# end
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'hackbox'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hackbox'
 end
