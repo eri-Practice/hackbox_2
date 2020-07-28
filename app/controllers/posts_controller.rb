11f09839cb4433c22841b75c6009f34a1f71bf7d
eri-Practice <64237498+eri-Practice@users.noreply.github.com>
2020-07-28 14:31:39 +0900

    投稿編集ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     if post.update(post_params)
-      redirect_to post_path(post.id)
+      redirect_to post_path(post.id), notice: '投稿を更新しました'
     else
       render :edit
     end
